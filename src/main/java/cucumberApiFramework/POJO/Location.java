eb28cffba45ff45270cea13be85e80d841592e69
bandyopadhyays <souni.banerjee.2015@gmail.com>
2020-02-14 00:26:17 +0530

    POJO classes for add place added

---
+++ src/main/java/cucumberApiFramework/POJO/Location.java
@@ -0,0 +1,20 @@
+package cucumberApiFramework.POJO;
+
+public class Location {
+
+	private int lat;
+	private int lang;
+	public int getLat() {
+		return lat;
+	}
+	public void setLat(int lat) {
+		this.lat = lat;
+	}
+	public int getLang() {
+		return lang;
+	}
+	public void setLang(int lang) {
+		this.lang = lang;
+	}
+	
+}
